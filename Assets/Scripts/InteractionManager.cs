59305c4877cedeaadd26b1565fd3cf97f05dd1f5
Frankie <f.joseph.stephens@gmail.com>
2018-09-29 14:14:07 +1200

    first

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor(typeof(InteractionManager))]
+public class EditorButtonScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        //this is my script
+        //from object being Inspected :
+        InteractionManager myscript = (InteractionManager)target;
+        if (GUILayout.Button("ButtonFunction"))
+        {
+            myscript.ButtonFunction();
+        }
+       
+    }
+}
+#endif
+
+public class InteractionManager : MonoBehaviour
+{
+    public enum PlayerInteractions { Sword, Boots, Shield, Helmet }
+    public PlayerInteractions RequiredInteraction;
+    public static InteractionManager Instance;
+    [System.Serializable]
+    public struct ColorForInteraction
+    {
+        public PlayerInteractions InteractionForColor;
+        public Color Color;
+    }
+    
+    public ColorForInteraction[] InteractionColors = new ColorForInteraction[4];
+
+    public PlayerButton[] PlayerButtons;
+    [Header("button things")]
+    public float TimeBetweenWaves = 0.2f;
+    public float TimeBetweenPixels=0.000001f;
+    public int PixelsPerRow = 2;
+    [Header("Player progress")]
+    public float CurrentAllowedTime = 1;
+    public float MaxTime=3, MinTime = 0.5f;
+    private float _currentTime = 0;
+    public float DifferencePerScore = 0.1f;
+
+    private int _iScore = 0;
+    public UnityEngine.UI.Text Score;
+    public UnityEngine.UI.Image TimeDisplay;
+    public bool Waiting = true;
+
+    private void Awake()
+    {
+        ButtonFunction();
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        Score.text = "0";
+        ResetProgress();
+    }
+
+
+    public Color GetColorForInteration(PlayerInteractions interaction)
+    {
+        return InteractionColors[(int)interaction].Color;
+    }
+
+   
+
+    public void ButtonPress(PlayerInteractions interaction)
+    {
+        Waiting = false;
+        if (RequiredInteraction == interaction)
+        {
+            //pass
+            //get new target interaction
+            RequiredInteraction = (PlayerInteractions)Random.Range(0,4);
+            Camera.main.backgroundColor = GetColorForInteration(RequiredInteraction);
+            _iScore += 1;
+            Score.text = _iScore.ToString();
+            //update timer
+            _currentTime = 0;
+            CurrentAllowedTime -= DifferencePerScore;
+            if (CurrentAllowedTime < MinTime)
+            {
+                CurrentAllowedTime = MinTime;
+            }
+        }
+        else
+        {
+            //gameover?
+            ResetProgress();
+
+           
+        }
+    }
+
+    private void ResetProgress()
+    {
+        Waiting = true;
+        CurrentAllowedTime = MaxTime;
+        _iScore = 0;
+    }
+
+    public void ButtonFunction()
+    {
+        for (int i = 0; i < PlayerButtons.Length; i++)
+        {
+            PlayerButtons[i].SetTextureColor(GetColorForInteration(PlayerButtons[i].Interaction));
+        }
+    }
+    private void Update()
+    {
+        if (!Waiting)
+        {
+            _currentTime += Time.deltaTime;
+            TimeDisplay.fillAmount = 1 - (_currentTime / CurrentAllowedTime);
+            if (_currentTime >= CurrentAllowedTime)
+            {
+                ResetProgress();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerButton.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using MEC;
+public class PlayerButton : MonoBehaviour, IPointerDownHandler
+{
+    public InteractionManager.PlayerInteractions Interaction;
+    public UnityEngine.UI.RawImage BGImage;
+    public Texture2D ImageTexture;
+    public Color Colorchange;
+    private float CurrentTime = 0;
+    bool _resetWave = false;
+    //what if you got all the pixels of x color and they reacted in a certain way :o
+    //the juice!
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Timing.RunCoroutine(ClickColorChange(Colorchange,true));
+        _resetWave = true;
+        InteractionManager.Instance.ButtonPress(Interaction);
+    }
+
+
+    public void SetTextureColor(Color color)
+    {
+        for (int x = 0; x < ImageTexture.width; x++)
+        {
+            for (int i = 0; i < ImageTexture.height; i++)
+            {
+                ImageTexture.SetPixel(x, i, color);
+            }
+        }
+        ImageTexture.Apply();
+    }
+
+    private void Update()
+    {
+        if (_resetWave)
+        {
+            CurrentTime += Time.deltaTime;
+            if (CurrentTime > InteractionManager.Instance.TimeBetweenWaves)
+            {
+                _resetWave = false;
+                CurrentTime = 0;
+                Timing.RunCoroutine(ClickColorChange(InteractionManager.Instance.GetColorForInteration(Interaction)));
+            }
+        }
+    }
+
+    private IEnumerator<float> ClickColorChange(Color color, bool lerp=false)
+    {
+        int DownX = ImageTexture.width / 2, UpX = ImageTexture.width / 2;
+        int DownY = ImageTexture.height / 2, UpY = ImageTexture.height / 2;
+        while (DownX >= 0 && DownY >= 0 && UpX <= ImageTexture.width && UpY <= ImageTexture.height)
+        {
+            //do all the rows of pixels required
+            for (int i = 0; i <= InteractionManager.Instance.PixelsPerRow; i++)
+            {
+                //bot row
+                for (int x = DownX; x < UpX; x++)
+                {
+                    if (lerp)
+                    //alt the color alpha by distance from middle
+                    color.a = Mathf.InverseLerp(0, ImageTexture.width / 2, DownY + i);
+
+                    ImageTexture.SetPixel(x, DownY + i, color);                   
+                }
+            }
+
+            //do all the rows of pixels required
+            for (int i = 0; i < InteractionManager.Instance.PixelsPerRow; i++)
+            {
+                //left column
+                for (int y = DownY; y <= UpY; y++)
+                {
+                    if (lerp)
+                        //alt the color alpha by distance from middle
+                        color.a = Mathf.InverseLerp(0, ImageTexture.width / 2, DownX + i);
+                    ImageTexture.SetPixel(DownX + i, y, color);
+                }
+            }
+            //do all the rows of pixels required
+            for (int i = 0; i < InteractionManager.Instance.PixelsPerRow; i++)
+            {
+                //top row
+                for (int x = DownX; x <= UpX; x++)
+                {
+                    if (lerp)
+                        //alt the color alpha by distance from middle
+                        color.a = Mathf.InverseLerp(ImageTexture.width, ImageTexture.width / 2, UpY - i);
+                    ImageTexture.SetPixel(x, UpY-i, color);
+                }
+            }
+            //do all the rows of pixels required
+            for (int i = 0; i < InteractionManager.Instance.PixelsPerRow; i++)
+            {
+                //right column
+                for (int y = DownY; y <= UpY; y++)
+                {
+                    if (lerp)
+                        //alt the color alpha by distance from middle
+                        color.a = Mathf.InverseLerp(ImageTexture.width, ImageTexture.width / 2, UpX - i);
+                    ImageTexture.SetPixel(UpX-i, y, color);
+                }
+            }
+            ////bot left
+            //ImageTexture.SetPixel(DownX, DownY, color);
+            ////top right
+            //ImageTexture.SetPixel(UpX, UpY, color);
+            ////top left
+            //ImageTexture.SetPixel(DownX, UpY, color);
+            ////bot right
+            //ImageTexture.SetPixel(UpX, DownY, color);
+            ImageTexture.Apply();
+            yield return Timing.WaitForSeconds(InteractionManager.Instance.TimeBetweenPixels);
+            DownX -= InteractionManager.Instance.PixelsPerRow;
+            DownY -= InteractionManager.Instance.PixelsPerRow;
+            UpX += InteractionManager.Instance.PixelsPerRow;
+            UpY += InteractionManager.Instance.PixelsPerRow;
+        }
+
+    }
+
+
+}
